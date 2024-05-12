3ea9bcb30f924f9ee1ed5323f8e558e986e79207
lukasz <lukol12321@gmail.com>
2024-05-12 21:37:00 +0200

    Separated View from app and added dependency injection

--- src/main/java/org/pwr/app/App.java
+++ src/main/java/org/pwr/app/App.java
@@ -11,80 +11,16 @@ import java.awt.*;
 import static org.pwr.app.CustomPanels.*;
 
 public class App {
-    private JFrame currentFrame;
+    private View view;
 
-    private JPanel setMainConfPanel() {
-        JPanel confPanel = new JPanel();
-        confPanel.setLayout(new GridLayout(12, 1));
-
-        SpinnerChangeListener spinnerChangeListener = new SpinnerChangeListener();
-        SliderChangeListener sliderChangeListener = new SliderChangeListener();
-        CheckBoxActionListener checkBoxActionListener = new CheckBoxActionListener();
-
-        confPanel.add(createSpinnerInputPanel("Liczba hakerów", 0, 100, spinnerChangeListener));
-        confPanel.add(createSpinnerInputPanel("Liczba Informatyków", 0, 100, spinnerChangeListener));
-        confPanel.add(createSpinnerInputPanel("Liczba pracowników", 0, 1000, spinnerChangeListener));
-        confPanel.add(createSliderInputPanel("Kompetencje Informatyków", 0, 100, sliderChangeListener));
-        confPanel.add(createSliderInputPanel("Kompetencje Hakerów", 0, 100, sliderChangeListener));
-        confPanel.add(createSpinnerInputPanel("Tempo utraty odporności", 0, 100, spinnerChangeListener));
-        confPanel.add(createCheckBoxInputPanel("Rozprzestrzenianie się Malwaru", checkBoxActionListener));
-        confPanel.add(createSpinnerInputPanel("Tempo rozprzestrzeniania się malwaru", 0, 100, spinnerChangeListener));
-
-        return confPanel;
-    }
-
-    private JSplitPane setMainRightSplitPlane() {
-        JSplitPane rightSplitPane = new JSplitPane();
-        rightSplitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
-        rightSplitPane.setDividerLocation(900);
-
-        JPanel mapPanel = setMainMapPanel();
-        JPanel statsPanel = setMainStatsPanel();
-
-        rightSplitPane.setTopComponent(mapPanel);
-        rightSplitPane.setBottomComponent(statsPanel);
-
-        return rightSplitPane;
-    }
-
-    private JPanel setMainMapPanel() {
-        JPanel mapPanel = new JPanel();
-        mapPanel.setLayout(new GridLayout(1, 1));
-
-        return mapPanel;
-    }
-
-    private JPanel setMainStatsPanel() {
-        JPanel statsPanel = new JPanel();
-        statsPanel.setLayout(new GridLayout(5, 1));
-
-        return statsPanel;
-    }
-
-    private JFrame setMainPage() {
-        this.currentFrame = new JFrame("Symulacja Ataku Hakerskiego");
-
-        Border border = BorderFactory.createMatteBorder(5, 5, 5, 5, Color.gray);
-        JSplitPane mainSplitPane = new JSplitPane();
-
-        mainSplitPane.setBorder(border);
-        mainSplitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
-        mainSplitPane.setDividerLocation(300);
-
-        JPanel confPanel = setMainConfPanel();
-        JSplitPane rightSplitPane = setMainRightSplitPlane();
-        mainSplitPane.setTopComponent(confPanel);
-        mainSplitPane.setBottomComponent(rightSplitPane);
-
-        currentFrame.add(mainSplitPane, BorderLayout.CENTER);
-        return currentFrame;
-    }
 
     private App() {
-        setMainPage();
-        currentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        currentFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        currentFrame.setVisible(true);
+        SliderChangeListener sliderChangeListener = new SliderChangeListener();
+        SpinnerChangeListener spinnerChangeListener = new SpinnerChangeListener();
+        CheckBoxActionListener checkBoxActionListener = new CheckBoxActionListener();
+
+        view = new View(spinnerChangeListener, sliderChangeListener, checkBoxActionListener);
+        view.run();
     }
 
     public static void main(String[] args) {
--- src/main/java/org/pwr/app/CustomPanels.java
+++ src/main/java/org/pwr/app/CustomPanels.java
@@ -5,10 +5,12 @@ import org.pwr.app.eventhandling.SliderChangeListener;
 import org.pwr.app.eventhandling.SpinnerChangeListener;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class CustomPanels {
-    public static JPanel createSpinnerInputPanel(String label, int min, int max, SpinnerChangeListener spinnerChangeListener) {
+    public static JPanel createSpinnerInputPanel(String label, int min, int max, ChangeListener spinnerChangeListener) {
         JPanel spinnerInputPanel = new JPanel();
         spinnerInputPanel.setLayout(new GridLayout(2, 1));
         SpinnerModel spinnerModel = new SpinnerNumberModel(1, min, max, 1); // initial value, min, max, step
@@ -23,7 +25,7 @@ public class CustomPanels {
         return spinnerInputPanel;
     }
 
-    public static JPanel createSliderInputPanel(String label, int min, int max, SliderChangeListener sliderChangeListener) {
+    public static JPanel createSliderInputPanel(String label, int min, int max, ChangeListener sliderChangeListener) {
         JPanel sliderInputPanel = new JPanel();
         sliderInputPanel.setLayout(new GridLayout(2, 1));
 
@@ -42,7 +44,7 @@ public class CustomPanels {
         return sliderInputPanel;
     }
 
-    public static JPanel createCheckBoxInputPanel(String label, CheckBoxActionListener checkBoxActionListener) {
+    public static JPanel createCheckBoxInputPanel(String label, ActionListener checkBoxActionListener) {
         JPanel checkBoxInputPanel = new JPanel();
         checkBoxInputPanel.setLayout(new GridLayout(2, 1));
 
